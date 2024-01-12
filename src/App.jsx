c8e65fdc403c28a053b293a5b3f20bd45f787d24
haaymonster <imhayleylee@gmail.com>
2024-01-12 23:36:10 +0900

    detail page init

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
 import "./App.css";
 import Main from "./pages/Main/Main/Main";
 import Login from "./pages/Login/Login";
+import Detail from "./pages/Detail/Detail";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       {/* 라우팅: path를 정하고 페이지 컴포넌트를 element에 넣어주면 됨 */}
       <Route path="/" element={<Main />}></Route>
       <Route path="/login" element={<Login />}></Route>
+      <Route path="/detail" element={<Detail />}></Route>
     </Routes>
   );
 }
