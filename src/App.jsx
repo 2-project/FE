4790f25c32e1905bd25b240c7f0e1f694900ce4b
haaymonster <imhayleylee@gmail.com>
2024-01-12 23:30:19 +0900

    fix init

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,17 @@ import {
   Outlet,
 } from "react-router-dom";
 import "./App.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    // element: <Navigate to="/Main" replace={true} />,
-  },
-  {
-    path: "/Main",
-    element: <Header />,
-    children: [
-      // { index: true, element: < /> },
-      // { path: "", element: < /> },
-    ],
-  },
-]);
+import Main from "./pages/Main/Main/Main";
+import Login from "./pages/Login/Login";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Routes>
+      {/* 라우팅: path를 정하고 페이지 컴포넌트를 element에 넣어주면 됨 */}
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
