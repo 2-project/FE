fb00861f4195c6b914fc7ec92277e541fe423057
haaymonster <imhayleylee@gmail.com>
2024-01-17 18:24:54 +0900

    add header and footer

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -4,26 +4,32 @@ import ProductInfo from "./components/ProductInfo";
 import AddToCart from "./components/AddToCart";
 import Description from "./components/Description";
 import ScrollToTop from "../../components/ScrollToTop";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const Detail = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        maxWidth: "760px",
-        margin: "0 55px 0 55px",
-      }}
-    >
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <ImageViewer></ImageViewer>
-        <div>
-          <ProductInfo></ProductInfo>
-          <AddToCart></AddToCart>
+    <div>
+      <Header></Header>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "760px",
+          margin: "50px 55px 0 25%",
+        }}
+      >
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <ImageViewer></ImageViewer>
+          <div>
+            <ProductInfo></ProductInfo>
+            <AddToCart></AddToCart>
+          </div>
         </div>
+        <Description></Description>
+        <ScrollToTop></ScrollToTop>
       </div>
-      <Description></Description>
-      <ScrollToTop></ScrollToTop>
+      <Footer></Footer>
     </div>
   );
 };
