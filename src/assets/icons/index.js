5a6d93c92932f045c79fcfd5cac4b1dfc95f5978
chaeyeon <kcy671524@gmail.com>
2024-01-18 15:08:28 +0900

    회원가입 폼 유효성 검사 추가 및 수정

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,1 +1,4 @@
 export { default as goSignUpIcon } from "./goSignUpIcon.svg";
+export { default as logoutIcon } from "./logout_icon.png";
+export { default as userBasicIcon_1 } from "./basic_profile_1.png";
+export { default as userBasicIcon_2 } from "./basic_profile_2.png";
