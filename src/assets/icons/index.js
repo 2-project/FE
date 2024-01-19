cd0f5b097f19994d6a08e204c6e1da823a6c81a6
chaeyeon <kcy671524@gmail.com>
2024-01-19 16:21:12 +0900

    글 수정

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,7 +2,7 @@
 export { default as goSignUpIcon } from "./goSignUpIcon.svg";
 export { default as userBasicIcon_1 } from "./basic_profile_1.png";
 export { default as userBasicIcon_2 } from "./basic_profile_2.png";
-// 메인 헤더에 쓰는 아이콘들 (로그인, 로그아웃, 유저, 장바구니, 물품관리 아이콘)
+// 메인 헤더에 쓰는 아이콘들 (로그인, 로그아웃, 유저, 장바구니, 물품관리 아이콘 순서)
 export { default as loginIcon } from "./login_icon.png";
 export { default as logoutIcon } from "./logout_icon.png";
 export { default as userIcon } from "./user_icon.png";
