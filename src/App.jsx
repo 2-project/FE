0a1e069c7603deafedb18ef4fea3476a1cbae716
Yes2eul <yeseul5611@gmail.com>
2024-01-19 15:52:12 +0900

    reset.css

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   Outlet,
 } from "react-router-dom";
 import "./App.css";
+import { Reset } from "styled-reset";
 import Main from "./pages/Main/Main/Main";
 import Login from "./pages/Login/Login";
 import PM from "./pages/PM/ProductManage";
@@ -18,17 +19,20 @@ import Cart from "./pages/Cart/Cart";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/product_manage" element={<PM />}></Route>
-      <Route path="/product_register" element={<ProductRegister />}></Route>
-      <Route path="/detail" element={<Detail />}></Route>
-      {/* <Route path="/login" element={<Cart />}></Route> */}
-      {/* <Route path="/login" component={Login} /> */}
-      <Route path="/order" component={Order} />
-      <Route path="/cart" component={Cart} />
-    </Routes>
+    <>
+      <Reset />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/product_manage" element={<PM />}></Route>
+        <Route path="/product_register" element={<ProductRegister />}></Route>
+        <Route path="/detail" element={<Detail />}></Route>
+        {/* <Route path="/login" element={<Cart />}></Route> */}
+        {/* <Route path="/login" component={Login} /> */}
+        <Route path="/order" component={Order} />
+        <Route path="/cart" component={Cart} />
+      </Routes>
+    </>
   );
 }
 
