1645f996272a421c92b4ac1485a086fb67298452
rose <113295030+ros-e2@users.noreply.github.com>
2024-01-21 17:29:27 +0900

    자동스크롤 및 페이크 스토어 api 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -6,22 +6,27 @@ import {
   Routes,
   Navigate,
   Outlet,
+  BrowserRouter,
 } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main/Main/Main';
 import Login from './pages/Login/Login';
+import PM from './pages/PM/ProductManage';
+import ProductRegister from './pages/PM/ProductRegister';
+import Detail from './pages/Detail/Detail';
 import Order from './pages/Order/Order';
 import Cart from './pages/Cart/Cart';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/login" element={<Cart />}></Route>
-      <Route path="/login" component={Login} />
-      <Route path="/order" component={Order} />
-      <Route path="/cart" component={Cart} />
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/product_manage" element={<PM />} />
+      <Route path="/product_register" element={<ProductRegister />} />
+      <Route path="/detail" element={<Detail />} />
+      <Route path="/order" element={<Order />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
   );
 }
