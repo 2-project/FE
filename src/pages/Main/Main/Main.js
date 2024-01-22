8cbb62c80b53019103ddbfca87491953b6bd4dbd
Yes2eul <yeseul5611@gmail.com>
2024-01-22 18:32:24 +0900

    main에 상품관리 버튼 추가

--- src/pages/Main/Main/Main.js
+++ src/pages/Main/Main/Main.js
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react';
-import ReactDOM from 'react-dom';
-import ImageSlider from './ImageSlider';
-import HeaderMain from './HeaderMain';
-import Footer from '../../../components/Footer/Footer';
-import ScrollToTop from '../../../components/ScrollToTop';
-import ProductListComponent from './ProductListComponent';
+import React, { useRef } from "react";
+import ReactDOM from "react-dom";
+import ImageSlider from "./ImageSlider";
+import HeaderMain from "./HeaderMain";
+import Footer from "../../../components/Footer/Footer";
+import ScrollToTop from "../../../components/ScrollToTop";
+import ProductListComponent from "./ProductListComponent";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Main() {
   const productListComponentRef = useRef();
@@ -16,14 +18,16 @@ function Main() {
   };
 
   return (
-    <div>
+    <>
       <HeaderMain onSubSectionClick={handleSubSectionClick} />
-
+      <Button style={{ float: "right" }} as={Link} to="/product_manage">
+        상품 관리
+      </Button>
       <ImageSlider></ImageSlider>
       <ProductListComponent ref={productListComponentRef} />
       <ScrollToTop></ScrollToTop>
       <Footer></Footer>
-    </div>
+    </>
   );
 }
 
